c6ebe76e193c9f5db0c580403b2d9d68f8c1fda7
ebony1105 <ebony1105@163.com>
2020-09-10 15:42:02 +0800

    创建Icon组件

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
-import x from 'icons/money.svg';
+import Icon from './Icon';
+
 
 const NavWrapper = styled.nav`
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -11,7 +12,15 @@ const NavWrapper = styled.nav`
     >li{
       width: 33.333%;
       text-align: center;
-      padding: 16px;
+      padding: 4px 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      .icon{
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
@@ -22,13 +31,15 @@ const Nav = ()=>{
     <NavWrapper>
       <ul>
         <li>
-          <img src={x} alt=""/>
+          <Icon name="tag"/>
           <Link to="/tags">标签</Link>
         </li>
         <li>
+          <Icon name="money"/>
           <Link to="/money">记账</Link>
         </li>
         <li>
+          <Icon name="chart"/>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
