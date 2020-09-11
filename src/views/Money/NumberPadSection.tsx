e958c92a904e60721d0252c6ae8ae95b3e566c32
ebony1105 <ebony1105@163.com>
2020-09-11 18:05:49 +0800

    NumberPad 基本功能实现

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -67,7 +67,20 @@ const Wrapper = styled.section`
 `;
 
 const NumberPadSection = ()=>{
-  const [output,setOutput] = useState('0');
+  const [output, _setOutput] = useState('0');
+
+  const setOutput = (output:string) => {
+    if (output.length > 16)
+    {
+      output = output.slice(0,16);
+    }
+    else if (output.length === 0)
+    {
+      output = '0';
+    }
+    _setOutput(output);
+  };
+
   const onClickButtonWrapper = (e:React.MouseEvent)=>
   {
     const text = (e.target as HTMLButtonElement).textContent;
@@ -98,13 +111,24 @@ const NumberPadSection = ()=>{
         }
         break;
       case '删除':
-        console.log('删除');
+        if (output.length === 1)
+        {
+          setOutput('');
+        }else {
+           setOutput(output.slice(0,-1))
+        }
         break;
       case '清空':
-        console.log('清空');
+        setOutput('');
         break;
       case '.':
-        console.log('.');
+        if (output.indexOf('.') >=0)
+        {
+          return;
+        }
+        else{
+          setOutput(output+'.')
+        }
         break;
       case 'OK':
         console.log('OK');
