93b4277bc57677b9846e45a23badbf5c80273fca
ebony1105 <ebony1105@163.com>
2020-09-11 20:50:51 +0800

    NumberPadSection 重构

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,70 +1,10 @@
 import styled from 'styled-components';
 import React, {useState} from 'react';
+import {Wrapper} from './NumberPadSection/Wrapper';
+import generate from '@babel/generator';
+import {generateOutput} from './NumberPadSection/generateOutput';
 
-const Wrapper = styled.section`
-  display: flex; 
-  //这里给的属性会影响后面的float：left； 
-  // 那样我们就不用给float：left 清除浮动了
-  flex-direction: column;
-  >.output{
-     background: white;
-     font-size: 36px;
-     line-height: 72px;
-     text-align:right;
-     padding: 0 16px;
-     box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
-                 inset 0 5px 5px -5px rgba(0,0,0,0.25); 
-       //上下都有阴影的实现 其中第一个是下阴影 先往上提5像素的阴影，再往下拉
-  }
-  >.pad{
-    //按钮一字排开 用flex会导致ok那一行和下一行出现空隙
-    // display:flex;
-    // flex-wrap: wrap;
-    >button{
-       font-size: 18px;
-       float: left;
-       width: 25%;
-       height: 64px;
-       border: none;
-       &.ok{
-          float: right;
-          height: 128px;      
-       }
-       &.zero{
-          width: 50%;
-       }
-       &:nth-child(1){
-          background: #f2f2f2;
-       }
-       &:nth-child(2),
-       &:nth-child(5){
-          background: #e0e0e0;
-       }
-       &:nth-child(3),
-       &:nth-child(6),
-       &:nth-child(9){
-          background: #d3d3d3;
-       }
-       &:nth-child(4),
-       &:nth-child(7),
-       &:nth-child(10){
-          background: #c1c1c1;
-       }
-       &:nth-child(8),
-       &:nth-child(11),
-       &:nth-child(13){
-          background: #b8b8b8;
-       }
-       &:nth-child(14)
-       {
-          background: #a9a9a9;
-       }
-       &:nth-child(12){
-          background: #9a9a9a;
-       }
-    }
-  }
-`;
+
 
 const NumberPadSection = ()=>{
   const [output, _setOutput] = useState('0');
@@ -90,49 +30,12 @@ const NumberPadSection = ()=>{
     {
       return;
     }
-    switch (text) {
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        if (output === '0')
-        {
-          setOutput(text);
-        }
-        else
-        {
-          setOutput(output+text)
-        }
-        break;
-      case '删除':
-        if (output.length === 1)
-        {
-          setOutput('');
-        }else {
-           setOutput(output.slice(0,-1))
-        }
-        break;
-      case '清空':
-        setOutput('');
-        break;
-      case '.':
-        if (output.indexOf('.') >=0)
-        {
-          return;
-        }
-        else{
-          setOutput(output+'.')
-        }
-        break;
-      case 'OK':
-        console.log('OK');
-        break;
+    if (text === 'OK') {
+      return;
+    }
+    if ('0123456789.'.split('').concat(['删除','清空']).indexOf(text) >= 0)
+    {
+      setOutput(generateOutput(text,output));
     }
   };
   return (
---
+++ src/views/Money/NumberPadSection/Wrapper.tsx
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  display: flex; 
+  //这里给的属性会影响后面的float：left； 
+  // 那样我们就不用给float：left 清除浮动了
+  flex-direction: column;
+  >.output{
+     background: white;
+     font-size: 36px;
+     line-height: 72px;
+     text-align:right;
+     padding: 0 16px;
+     box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
+                 inset 0 5px 5px -5px rgba(0,0,0,0.25); 
+       //上下都有阴影的实现 其中第一个是下阴影 先往上提5像素的阴影，再往下拉
+  }
+  >.pad{
+    //按钮一字排开 用flex会导致ok那一行和下一行出现空隙
+    // display:flex;
+    // flex-wrap: wrap;
+    >button{
+       font-size: 18px;
+       float: left;
+       width: 25%;
+       height: 64px;
+       border: none;
+       &.ok{
+          float: right;
+          height: 128px;      
+       }
+       &.zero{
+          width: 50%;
+       }
+       &:nth-child(1){
+          background: #f2f2f2;
+       }
+       &:nth-child(2),
+       &:nth-child(5){
+          background: #e0e0e0;
+       }
+       &:nth-child(3),
+       &:nth-child(6),
+       &:nth-child(9){
+          background: #d3d3d3;
+       }
+       &:nth-child(4),
+       &:nth-child(7),
+       &:nth-child(10){
+          background: #c1c1c1;
+       }
+       &:nth-child(8),
+       &:nth-child(11),
+       &:nth-child(13){
+          background: #b8b8b8;
+       }
+       &:nth-child(14)
+       {
+          background: #a9a9a9;
+       }
+       &:nth-child(12){
+          background: #9a9a9a;
+       }
+    }
+  }
+`;
+
+export {Wrapper};
