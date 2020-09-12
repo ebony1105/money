0937b4dd5b7c135bd43313fb7a34741d4dc3d3c5
ebony1105 <ebony1105@163.com>
2020-09-12 22:19:56 +0800

    update tag页面

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import {useTags} from '../useTags';
+import {useParams} from 'react-router';
 
-// @ts-ignore
-const Tag : React.FC = () =>
+
+type Params = {
+  id:string
+};
+
+const Tag : React.FC = (props) =>
 {
+  const {findTag} = useTags();
+  let {id} =useParams<Params>();
+  const tag = findTag(parseInt(id));
   return (
-    <div>hi</div>
+    <div>{div.name}</div>
   );
 };
 
